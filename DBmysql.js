979923acc10f92d9483fe1a49ca70379f66ebeed
unknown <shanibh38@gmail.com>
2019-04-12 00:48:33 +0300

    sa

--- DBmysql.js
+++ DBmysql.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "quizbgdm.coxqxrjsrdck.us-east-1.rds.amazonaws.com",
+  host: "quizbgdmb.coxqxrjsrdck.us-east-1.rds.amazonaws.com",
   port: "3306",
   user: "admin",
   password: "admingdmb",
